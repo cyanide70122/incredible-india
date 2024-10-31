74ce2b36d6725405dc9ba9635d6c8431f3f76ec0
cyanide70122 <rachanjadav18@gmail.com>
2024-10-31 20:10:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className= "w-100 bg-cyan-300">
+      <p className="text-3xl font-bold underline"> hello</p>
+    </div>
+  )
+}
+
+export default App
